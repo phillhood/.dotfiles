94b58a5041cf4c23bcd70a9bd05bea0005be6e7f
Phill Hood <phill@phillhood.ca>
2026-01-30 19:14:08 -0500

    update

---
+++ .config/tmux-powerline/config.sh
@@ -0,0 +1,19 @@
+# tmux-powerline configuration
+# See: https://github.com/erikw/tmux-powerline
+
+# Use Material Monokai theme
+export TMUX_POWERLINE_THEME="material-monokai"
+
+# Path to custom themes directory
+export TMUX_POWERLINE_DIR_USER_THEMES="$HOME/.config/tmux-powerline/themes"
+
+# Patched font for powerline symbols (uses Nerd Font glyphs)
+export TMUX_POWERLINE_PATCHED_FONT_IN_USE="true"
+
+# Status bar settings
+export TMUX_POWERLINE_STATUS_JUSTIFICATION="left"
+export TMUX_POWERLINE_STATUS_INTERVAL=5
+
+# Segment settings (optional customization)
+# export TMUX_POWERLINE_SEG_WEATHER_LOCATION="your_location"
+# export TMUX_POWERLINE_SEG_WEATHER_UNIT="c"
