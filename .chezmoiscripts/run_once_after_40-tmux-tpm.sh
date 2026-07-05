e09c19a86e7679d8b232115ed0948989ea66e868
Phill Hood <phill@phillhood.ca>
2026-07-05 06:23:05 -0400

    remove chezmoi machinery; rename _utils to tools

--- .chezmoiscripts/run_once_after_40-tmux-tpm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-tpm_dir="$HOME/.tmux/plugins/tpm"
-if [ ! -d "$tpm_dir" ]; then
-  echo "==> Cloning tpm to $tpm_dir"
-  git clone --depth 1 https://github.com/tmux-plugins/tpm "$tpm_dir"
-fi
